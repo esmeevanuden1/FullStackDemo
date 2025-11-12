4489f2cbef41d2198a7d5d6e8ebcae708dd2ed61
Julian Vink <julianvink@acknowledge.nl>
2025-11-12 12:02:35 +0100

    Checkt nu de tabel

--- jarchetype.py
+++ jarchetype.py
@@ -1,3 +1,33 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+J_HOST = "127.0.0.1"
+J_PORT = "3306"
+J_USER = "root"
+J_PASSWORD = ""
+J_NAME = "yugiohdb"
+
 def start():
-    print("Jarchetype started")
+    load_dotenv()
+    print(list_table())
     return True
+
+def get_db():
+    connection = mysql.connector.connect(
+        host=J_HOST,
+        port=J_PORT,
+        user=J_USER,
+        password=J_PASSWORD,
+        database=J_NAME
+    )
+    return connection
+
+def list_table():
+    connection = get_db()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM archetype;")
+    rows = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return rows
